2d28c5f02a98157b814e1e37849acc656f414c5b
RAJ <RAJ@p-PC>
2020-08-12 11:18:52 +0530

    Batch : Ashutosh-Tester at 11:18 A.M.

---
+++ Hello_World/hello.c
@@ -0,0 +1,16 @@
+/*
+ * hello.c
+ *
+ *  Created on: Aug 12, 2020
+ *      Author: RAJ
+ */
+#include<stdio.h>
+int main()
+{
+	int a=3,b=4,s;
+	s=a+b;
+	printf("HELLO WORLD !!!!!!");
+	printf("\n%d",s);
+return 0;
+}
+
