8d951c7b5d199cfea8baf5ba2c20d13eef5ceac5
Ashutosh-Tester <muskelon1111@gmail.com>
2020-08-16 21:25:18 +0530

    First Commit

---
+++ Hello_World/intro.cpp
@@ -0,0 +1,16 @@
+/*
+ * intro.cpp
+ *
+ *  Created on: Aug 12, 2020
+ *      Author: RAJ
+ */
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=3,b=4,s;
+	s=a+b;
+	cout<<"hello world"<<" "<<s<<endl;
+}
+
+
