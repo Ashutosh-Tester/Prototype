e2e63936c72dbb5ffab9257ec38a310963b4e973
Ashutosh-Tester <muskelon1111@gmail.com>
2020-09-27 15:30:30 +0530

    First Commit

---
+++ College_Work/stack_functions.c
@@ -0,0 +1,85 @@
+#include<stdio.h>
+#define Size 5
+int stack[Size];
+int top=-1;
+int push(int data)
+{
+if(top<Size-1)
+{
+	if(top<0)
+	{
+		top=0;
+		stack[top]=data;
+	}
+	else{
+		stack[top+1]=data;
+		top++;
+	}
+}
+else
+{
+	printf("Stack Overflow\n");
+}
+}
+void peep()
+ {
+ 	int c;
+ 	if(top>=0)
+ 	{
+ 		c=stack[top];
+ 		printf("%d",c);
+ 	}
+ 	else{
+ 		printf("Stack Is Empty");
+ 	}
+ }
+void display()
+{
+	//printf("%d\n",top);
+	for(int i=0;i<Size;i++)
+	{
+		printf("%d ",stack[i]);
+	}
+}
+int pop()
+{
+	if(top>=0)
+	{
+		
+		int n=stack[top];
+		top--;
+		return n;
+	}
+
+
+ }
+
+int main(void)
+{
+ int a;
+ while(a!=5){
+ printf("\nEnter 1 for push\nEnter 2 for dislpay\nEnter 3 for pop\nEnter 4 for peek\nEnter Enter the choice\n");
+ scanf("%d",&a);
+ switch(a){
+ 	case 1:printf("\nEnter the elements in the stack\n");
+ 	int ele;
+ 	for(int i=0;i<Size;i++){
+ 	scanf("%d",&ele);
+    push(ele);}
+    break;
+    case 2:printf("\nElements in the stack : ");
+    display();
+    break;
+    case 3:printf("Elements in the stack : ");
+    display();
+     int h=pop();
+    printf("\nPopped Element : %d",h);
+    break;
+    case 4:printf("The Peeked Element is : ");
+    peep();
+    break;
+  default: printf("Enter correct choice");
+ }
+}
+  return 0;  
+}
