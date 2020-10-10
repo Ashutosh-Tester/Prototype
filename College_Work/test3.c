f143fdab6054a8e311e8b2d08a3128f30981857b
Ashutosh-Tester <muskelon1111@gmail.com>
2020-10-10 12:43:13 +0530

    First Commit

--- College_Work/test3.c
+++ College_Work/test3.c
@@ -1,26 +1,126 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define size 5;
+//#define Size 5
 struct demo{
-	int a;
-struct demo *q;
-};
-int main()
-{ 
-	struct demo *head=NULL;
-	//struct demo *first=NULL;
-	struct demo *second=NULL;
-	struct demo *third=NULL;
-	head=(struct demo*)malloc(sizeof(struct demo));
-	second=(struct demo*)malloc(sizeof(struct demo)); 
-	third=(struct demo*)malloc(sizeof(struct demo));  
-	struct demo* n;
-	n=head;
-	while(n!=NULL)
+	int data;
+struct demo *next;
+};struct demo*head=NULL;
+void linkedlist()
+{
+	struct demo *node,*n;
+	node=(struct demo*)malloc(sizeof(struct demo));
+     if(node==NULL)
+     {
+     	printf("List is Empty");
+     }
+     printf("Enter elements in Linked List\n");
+     	scanf("%d",&node->data);
+     	node->next=NULL;
+    
+     if(head==NULL)
+     {
+     	head=node;
+     }
+     else{
+     	n=head;
+     	while(n->next!=NULL)
+     	{
+     		n=n->next;
+     	}
+     	n->next=node;}   
+}
+void show()
+{
+	struct demo* node;
+	node=head;
+	if(node==NULL)
 	{
-		printf(" %d ",n->a);
-		n=n->q;
+		printf("list is empty");
 	}
-	int s=sizeof(head);
-	printf("\n%d",s);
+	else{
+	printf("\nElements in the List : ");
+	while(node->next!=NULL)
+	{
+		printf("%d ",node->data);
+		node=node->next;
+	}}
+	printf("%d ",node->data);
+}
+void delete_end()
+{
+	struct demo* node,*n;
+	if(node==NULL)
+	{
+		printf("List is Empty");
+	}
+	else if(head->next==NULL)
+	{
+		n=head;
+		head=NULL;
+		printf("\nThe deleted element is = %d\n",n->data);
+		free(n);
+
+	}
+	else
+	{
+		n=head;
+		while(n->next!=NULL)
+		{
+			node=n;
+			n=n->next;
+
+		}
+		printf("\nThe Deleted element is = %d\n",n->data);
+		node->next=NULL;
+		free(n);
+	}
+
+}
+void delete_begin()
+{
+	struct demo *node,*n;
+	if(head==NULL)
+	{
+		printf("\nList is Empty");
+	}
+	else
+	{
+		n=head;
+		head=head->next;
+		printf("\nThe deleted element is = %d ",n->data);
+	}
+}
+void delete_pos()
+{struct demo *node,*n;
+	int pos;
+	printf("\nEnter the position to be deleted\n");
+	scanf("%d",&pos);
+    n=head;
+	for(int i=0;i<pos-1;i++)
+	{
+		node=n;
+		n=n->next;
+		if(n==NULL)
+		{
+			printf("Error");
+		}
+	}
+	printf("\nDeleted Element = %d",n->data);
+	node->next=n->next;
+	free(n);
+}
+int main(void)
+{ int Size=0;
+	printf("Enter size of Linked List\n");
+	scanf("%d",&Size);
+	for(int i=0;i<Size;i++){
+	linkedlist();}
+      show(); 
+      // delete_end();
+      // show();
+      // delete_begin();
+      // show();
+      delete_pos();
+      show();
+
 }
---
+++ College_Work/test4.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Node
+{
+	int data;
+	struct Node *next;
+};struct Node *top;
+void push(int data)
+{
+	struct Node* node,*n;
+	node=(struct Node*)malloc(sizeof(struct Node));
+	// printf("\nEnter element in the stack\n");
+	// scanf("%d",&node->data);
+if(top==NULL)
+{  top=(struct Node*)malloc(sizeof(struct Node));
+	top->data=data;
+	top->next=NULL;
+}
+else{
+    node->data=data;
+	node->next=top;
+	top=node;}
+}
+void display()
+{
+	struct Node* n;
+	n=top;
+	if(top==NULL)
+	{
+		printf("\nStack is Empty\n");
+	}
+	else
+	{
+		while(n->next!=NULL){
+		printf("%d -> ",n->data);
+		n=n->next;}
+		printf("%d ",n->data);
+	}
+}
+int main(void)
+{
+	int i,size;
+	// printf("Enter size of stack\n");
+	// scanf("%d",&size);
+	push(1);
+	push(2);
+	push(3);
+	push(4);
+	push(5);
+	display();
+
+
+}
