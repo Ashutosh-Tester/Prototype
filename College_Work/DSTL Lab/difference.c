27a4cf880bbde4f848ab7e8f8fc863feda580d51
Ashutosh-Tester <muskelon1111@gmail.com>
2020-11-28 17:45:02 +0530

    Batch : AshutoshTester 28/11/2020

--- College_Work/DSTL Lab/difference.c
+++ College_Work/DSTL Lab/difference.c
@@ -23,16 +23,50 @@ int difference(int a[],int b[],int m,int n)
 	{
 		c[k++]=a[i++];
 	}
-	printf("\nThe difference of two sets : ");
+	printf("\nThe difference of sets A-B : ");
 	for(i=0;i<k;i++){
 	printf("%d ",c[i]);}
 
 }
+int difference1(int a[],int b[],int m,int n)
+{
+	int i=0,j=0,k=0;
+	int d[m+n];
+	while(i<m && j<n)
+	{
+		if(a[i]<b[j]){
+
+			i++;
+		}
+		else if(b[j]<a[i]){
+			d[k++]=b[j];
+			j++;
+		}
+		else{
+			i++;
+			j++;
+
+		}
+
+	}
+	while(j<n)
+	{
+		d[k++]=b[j++];
+	}
+	printf("\nThe difference of sets B-A : ");
+	for(i=0;i<k;i++){
+	printf("%d ",d[i]);}
+
+
+}
 int main(void)
 {
-	int a[]={1,3,4,5,6,7,9,10,13};
-	int b[]={3,5,7,9,11,12};
+	// int a[]={1,3,4,5,6,7,9,10,13};
+	// int b[]={3,5,7,9,11,12,14};
+	int a[]={1,2,3,4,5,6,8};
+	int b[]={2,3,5,6,7};
 	int size=sizeof(a)/sizeof(int);
 	int size1=sizeof(a)/sizeof(int);
     difference(a,b,size,size1);
+    difference1(a,b,size,size1);
 }
--- College_Work/Sorting/merge_sort.c
+++ College_Work/Sorting/merge_sort.c
@@ -1,15 +1,51 @@
 #include<stdio.h>
+int merge(int a[],int low,int mid,int high)
+{
+	int i,j,k;
+	i=low;j=mid+1;k=low;
+	int b[50];
+	while(i<=mid && j<=high)
+	{
+		if(a[i]<a[j])
+		{
+            b[k++]=a[i];
+            i++;
+		}
+			else{
+				b[k++]=a[j];
+				j++;
+			}
+	}
+	while(i<=mid)
+	{
+		b[k++]=a[i];
+		i++;
+	}
+	while(j<=high)
+	{
+		b[k++]=a[j];
+		j++;
+	}
+	for(int i=0;i<k;i++)
+		{
+			a[i]=b[i];}
+	
+}
 int sort(int a[],int low,int high)
 {
-   int m=(low+high)/2;
-	sort(a,l,m);
-	sort(a,m+1,h);
-	merge(a,);
-}
-int merge()
-{
+	int mid;
+	if(low<high){
+    mid=(low+high)/2;
+	sort(a,low,mid);
+	sort(a,mid+1,high);
+	merge(a,low,mid,high);}
 	
-
+}
+int display(int a[],int size)
+{
+  for(int i=0;i<size;i++)
+		{
+			printf("%d ",a[i]);}
 }
 int main(void)
 {
@@ -18,5 +54,9 @@ int main(void)
 	size=sizeof(a)/sizeof(int);
 	l=0;
 	h=size-1;
+	printf("\nArray before Sorting : ");
+	display(a,size);
 	sort(a,l,h);
+	printf("\nArray before Sorting : ");
+	display(a,size);
 }
